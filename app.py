bf26ed1dbe659a3c5d34c5bf37082fc67006c28f
Anurag Dhyani <anuragdhyani01@gmail.com>
2021-04-23 13:54:05 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,27 +1,48 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect , url_for
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import InputRequired, Email, Length
 from flask_sqlalchemy import SQLAlchemy
+import os
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
 
+file_path = os.path.abspath(os.getcwd())+"\database.db"
 app = Flask(__name__)
 app.config['SECRET_KEY']='Thisissecret'
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:////mnt/c/Users/Anurag/Desktop/Major_Project/Healthcare/database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+file_path
 Bootstrap(app)
 db=SQLAlchemy(app)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
+class User(UserMixin, db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    username=db.Column(db.String(15),unique=True)
+    email=db.Column(db.String(50),unique=True)
+    password=db.Column(db.String(80))
+
+@login_manager.user_loader
+
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
 class LoginForm(FlaskForm):
     username=StringField('username',validators=[InputRequired(),Length(min=4,max=15)])
-    password=PasswordField('password',validators=[InputRequired(),Length(min=4,max=80)])
+    password=PasswordField('password',validators=[InputRequired(),Length(min=8,max=80)])
     remember=BooleanField('rememberme')
 
 
 class RegisterForm(FlaskForm):
     username=StringField('username',validators=[InputRequired(),Length(min=4,max=15)])
     email=StringField('email',validators=[InputRequired(),Email(message='Invalid Email'),Length(max=50)])
-    password=PasswordField('password',validators=[InputRequired(),Length(min=4,max=80)])
+    password=PasswordField('password',validators=[InputRequired(),Length(min=8,max=80)])
 
 @app.route('/')
 def index():
@@ -31,19 +52,40 @@ def index():
 def login():
     form=LoginForm()
     if form.validate_on_submit():
-        return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
+        user=User.query.filter_by(username=form.username.data).first()
+        if user:
+            if check_password_hash(user.password,form.password.data):
+                login_user(user, remember=form.password.data)
+                return redirect(url_for('dashboard'))
+        return '<h1> Invalid Username or Password </h1> '
+        # return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
+
     return render_template('login.html',form=form)
 
 @app.route('/signup',methods=['GET', 'POST'])
 def signup():
     form=RegisterForm()
     if form.validate_on_submit():
-        return '<h1>' + form.username.data + ' ' + form.email.data + ' ' + form.password.data + '</h1>'
+        hashed_password = generate_password_hash(form.password.data,method='sha256')
+        new_user=User(username=form.username.data,email=form.email.data,password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return ' <h1> New User has been Created </h1> ' 
+        # return '<h1>' + form.username.data + ' ' + form.email.data + ' ' + form.password.data + '</h1>'
     return render_template('signup.html',form=form)
 
 @app.route('/dashboard')
+@login_required
 def dashboard():
-    return render_template('dashboard.html')
+    return render_template('dashboard.html',name=current_user.username)
+
+@app.route('/logout')
+@login_required
+
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
